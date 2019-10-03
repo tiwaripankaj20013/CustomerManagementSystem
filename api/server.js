886e222d032534ca960ca570791bcc91a9be086e
Palvi Dhawan <palvidhawan8@gmail.com>
2019-10-04 01:33:29 +0530

    Changes in API Folder to resolve CORS

--- api/server.js
+++ api/server.js
@@ -1,10 +1,20 @@
 let app = require('express');
 let bodyParser = require('body-parser');
 
+
 let server = app();
+
+server.use(function(req, res, next){
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+})
+
 server.use(bodyParser.json());
 
 let customer = require('./customerdata.json');
+
 //read all books
 server.get('/api/customer/all',function(req,res){
     console.log("Just running all REST URI!!");
