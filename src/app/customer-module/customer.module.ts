e137f97d8fa6f514cff9c61f92406b967ea3208f
Sahil Chhabra <sahilchhabra016@gmail.com>
2019-10-05 03:45:32 +0530

    header module complete

--- src/app/customer-module/customer.module.ts
+++ src/app/customer-module/customer.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CustomerCardComponent } from './customer-card/customer-card.component';
 import { CustomerComponent } from './customer-card/customer/customer.component';
-
+import { CustomerHeaderComponent } from './customer-header/customer-header.component';
 @NgModule({
     imports: [
     BrowserModule,
   ],
   declarations: [
     CustomerCardComponent,
-    CustomerComponent
+    CustomerComponent,
+    CustomerHeaderComponent
   ],
   exports:[
-    CustomerCardComponent
+    CustomerCardComponent,
+    CustomerHeaderComponent
   ],
   providers: [],
   bootstrap: [CustomerCardComponent]
